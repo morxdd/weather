7154b2ca4891867e392652898ee4e6f1100b67af
morxdd <pomin1995qoot@gmail.com>
2024-10-27 11:33:26 +0800

    - 新增 switchWeatherIcon 函式，根據天氣型態更換相對應的圖示 - 增加錯誤處理邏輯，設置預設圖示顯示 - 修正 weatherTypeNumber 的邏輯問題，確保圖示顯示正確

--- 01.js
+++ 01.js
@@ -15,7 +15,7 @@ function fetchWeatherData(apiUrl) {
         })
         .catch(error => {
             console.error('發生錯誤：', error);
-            
+
         })
 }
 // 處理搜尋邏輯
@@ -24,7 +24,7 @@ function handleSearch() {
     console.log(`使用者輸入的縣市名: ${userInput}`);
 
     if (weatherData) {
-        let locationIndex = weatherData.findIndex(location => location.locationName.toLowerCase()  === userInput);
+        let locationIndex = weatherData.findIndex(location => location.locationName.toLowerCase() === userInput);
         if (weatherData[locationIndex]) {
             console.log(`找到的縣市與索引： ${weatherData[locationIndex].locationName}、[${locationIndex}]`);
             updateDOM(locationIndex);
@@ -32,7 +32,7 @@ function handleSearch() {
             // alert('請輸入正確縣市名')
             const locationNameElement = document.getElementById('location-name');
             locationNameElement.innerText = '請輸入正確縣市名';
-            locationNameElement.style ="color: #e55"
+            locationNameElement.style = "color: #e55"
         }
     } else {
         console.error('資料載入失敗或尚未載入');
@@ -40,16 +40,16 @@ function handleSearch() {
 }
 
 // 更新 DOM 的函式
-function updateDOM(locationIndex){
+function updateDOM(locationIndex) {
     const locationNameElement = document.getElementById('location-name');
     const weatherTypeElement = document.getElementById('weather-type');
     const rainProbabilityElement = document.querySelector('#rain-probability .en-text span')
     const comfortIndexElement = document.querySelector('#comfort-index .en-text');
     const temperatureElement = document.querySelector('#temperature span');
-    const weatherIconElement = document.getElementById('weather-icon');
+
     const selectedLocation = weatherData[locationIndex];
     console.log(temperatureElement);
-    
+
 
     //顯示目前資料
     console.log(selectedLocation); //當前城市
@@ -63,12 +63,13 @@ function updateDOM(locationIndex){
     // 將資料封包成物件
     let selectedLocationInfo = {
         "weatherType": selectedLocation.weatherElement[0].time[0].parameter.parameterName,
+        "weatherTypeNumber": selectedLocation.weatherElement[0].time[0].parameter.parameterValue,
         "rainProbability": selectedLocation.weatherElement[1].time[0].parameter.parameterName,
         "comfortIndex": selectedLocation.weatherElement[3].time[0].parameter.parameterName,
         "temperature": selectedLocation.weatherElement[4].time[0].parameter.parameterName
     }
     console.log(selectedLocationInfo.temperature);
-    
+
 
     // 更新 DOM 內容
     locationNameElement.innerText = selectedLocation.locationName;
@@ -76,6 +77,8 @@ function updateDOM(locationIndex){
     rainProbabilityElement.innerText = selectedLocationInfo.rainProbability;
     comfortIndexElement.innerText = selectedLocationInfo.comfortIndex;
     temperatureElement.innerText = selectedLocationInfo.temperature;
+
+    switchWeatherIcon(selectedLocationInfo)
 }
 
 
@@ -85,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(() => {
             let locationIndex = 5
             updateDOM(locationIndex)
-            
+
             // 綁定搜尋按鈕的事件
             searchBtn.addEventListener('click', function () {
                 handleSearch();
@@ -99,11 +102,61 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         })
         .catch(() => {
-            
+
             console.error('資料載入失敗');
-            alert('失敗')
         });
 });
 
 // 修改 icon 的邏輯
+function switchWeatherIcon(selectedLocationInfo) {
+    const weatherIconElement = document.getElementById('weather-icon');
+    if (selectedLocationInfo.weatherType) {
+        console.log(selectedLocationInfo.weatherType);
+        console.log(selectedLocationInfo.weatherTypeNumber);
+        const weatherTypeNumber = Number(selectedLocationInfo.weatherTypeNumber);
+        switch (weatherTypeNumber) {
+            case 1:
+                weatherIconElement.src = "weather-img/wx-icon/01-sunny.svg";
+                break;
+            case 2:
+                weatherIconElement.src = "weather-img/wx-icon/02-partly-cloudy.svg";
+                break;
+            case 3:
+                weatherIconElement.src = "weather-img/wx-icon/03-mostly-cloudy.svg";
+                break;
+            case 4:
+                weatherIconElement.src = "weather-img/wx-icon/04-cloudy.svg";
+                break;
+            case 5:
+                weatherIconElement.src = "weather-img/wx-icon/05-cloudy-to-overcast.svg";
+                break;
+            case 6:
+                weatherIconElement.src = "weather-img/wx-icon/06-overcast-to-cloudy.svg";
+                break;
+            case 7:
+                weatherIconElement.src = "weather-img/wx-icon/07-overcast.svg";
+                break;
+            case 8:
+                weatherIconElement.src = "weather-img/wx-icon/08-cloudy-showers.svg";
+                break;
+            case 9:
+                weatherIconElement.src = "weather-img/wx-icon/09-cloudy-showers.svg";
+                break;
+            case 10:
+                weatherIconElement.src = "weather-img/wx-icon/10-overcast-showers.svg";
+                break;
+            case 11:
+                weatherIconElement.src = "weather-img/wx-icon/11-overcast-occasional-showers.svg";
+                break;
+            case 14:
+                weatherIconElement.src = "weather-img/wx-icon/14-overcast-rain.svg";
+                break;
+            default:
+                weatherIconElement.src = "weather-img/wx-icon/default.svg"; // 預設 icon
+        }
+    } else {
+        console.error('資料載入失敗')
+    }
 
+
+}
